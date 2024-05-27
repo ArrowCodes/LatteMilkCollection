50fc52d8ffaf09313c2136db526d3649a5085bd3
ArrowCodes <arrowcodes365@gmail.com>
2024-05-27 10:59:51 +0300

    Still in construction :hammer:

---
+++ app/src/main/java/com/enlace/lattemilkcollection/AddFarmersActivity.java
@@ -0,0 +1,14 @@
+package com.enlace.lattemilkcollection;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddFarmersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_farmers);
+    }
+}
--- app/src/main/java/com/enlace/lattemilkcollection/FarmersActivity.java
+++ app/src/main/java/com/enlace/lattemilkcollection/FarmersActivity.java
@@ -5,15 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class FarmersActivity extends AppCompatActivity {
-
+   FloatingActionButton fab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_farmers);
         getSupportActionBar().setTitle("Farmers");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //initializa fab
+        fab = findViewById(R.id.fab);
+        fab.setOnClickListener(view -> {
+            Intent i = new Intent(getApplicationContext(),AddFarmersActivity.class);
+            startActivity(i);
+        });
     }
 
     public boolean onOptionsItemSelected(MenuItem item) {
